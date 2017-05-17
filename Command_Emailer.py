419d8ca06d5afb20d064ad170ac530d2c693ad85
Trevor Gahl <tgahl7@gmail.com>
2017-05-17 14:22:34 -0600

    Adding modification suggestion comments

--- Command_Emailer.py
+++ Command_Emailer.py
@@ -37,6 +37,9 @@ def send(sendFile):
     server.sendmail(fromaddr, toaddr, text)
 
 def imeiSelection(menuChoice):
+
+    #### Pull from IMEI outlined in main window
+
     if(menuChoice == 1):
         IMEI = str(300234060252680)
     elif(menuChoice == 2):
@@ -51,6 +54,9 @@ print("2: 300234063043420\n")
 firstChoice = int(raw_input())
 imeiSelection(firstChoice)
 
+
+        #### Modify to only display cutdown and idle in the GUI
+
 print("\n1: Send Valve Open Command\n")
 print("2: Send Fan Start Command\n")
 print("3: Send Fan Off Command\n")
@@ -58,6 +64,8 @@ print("4: Send Valve Close Command\n")
 print("5: Send Nichrome Cutdown Command\n")
 menuSelect = int(raw_input())
 
+
+        #### Use GUI buttons instead of command line input
 if(menuSelect==1):
     fileOut = 'cutdown.sbd'
     send(fileOut)
@@ -77,41 +85,3 @@ else:
     print("Invalid Selection sending Idle command")
     fileOut = 'idle.sbd'
     send(fileOut)
-'''
-else:
-    wait = True
-    path = os.getcwd()
-    for file in os.listdir(path):
-        if file.endswith(".sbd"):
-            wait = True
-            # print(file)
-            command = str(file)
-            print("Sendng: %s" % command)
-            fromaddr = "msgc.borealis@gmail.com"
-            toaddr = "data@sbd.iridium.com"
-            msg = MIMEMultipart()
-            msg['From'] = fromaddr
-            msg['To'] = toaddr
-            msg['Subject'] = IMEI
-            part = MIMEBase('application', "octet-stream")
-            part.set_payload(open(command, "rb").read())
-            Encoders.encode_base64(part)
-
-            part.add_header('Content-Disposition','attachment; filename=%s' % command)
-
-            body = ""
-            msg.attach(MIMEText(body, "plain"))
-            msg.attach(part)
-
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.ehlo()
-            server.login("msgc.borealis", "FlyHighN0w")
-            text = msg.as_string()
-            server.sendmail(fromaddr, toaddr, text)
-            while(wait == True):
-                nxt_key = str(raw_input())
-                if(nxt_key == 'n'):
-                    wait = False
-'''
