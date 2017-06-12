f601c68e84cc6da59c5e7670ced850b2c155192f
trevor-gahl <tgahl7@gmail.com>
2017-06-12 11:55:22 -0600

    Fixed import

--- GetData.py
+++ GetData.py
@@ -3,11 +3,14 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
 from BalloonUpdate import *
+from time import sleep
 import MySQLdb
 import datetime
 import serial
 import threading
 import json
+import time
+
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
